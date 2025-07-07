86e424776abd46e8b0e0a3dd0b1eeb7ecd8a438d
kevynnmc <cardosojuandavid123@gmail.com>
2025-07-07 14:49:59 -0500

    Hackerrank3.py

--- Hackerrank3.py
+++ Hackerrank3.py
@@ -4,9 +4,3 @@ def pageCount(n, p):
     from_back = (n // 2) - (p // 2)
     return min(from_front, from_back)
 
-#para visual
-n = int(input("Introduce el número total de páginas: "))
-p = int(input("Introduce la página que quieres abrir: "))
-
-resultado = pageCount(n, p)
-print(f"Número mínimo de movimientos: {resultado}")
