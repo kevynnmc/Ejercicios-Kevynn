f879378962172a0507427b5f0abd8e800ac1e747
kevynnmc <cardosojuandavid123@gmail.com>
2025-07-07 14:53:48 -0500

    Hackerrank3.py

--- Hackerrank3.py
+++ Hackerrank3.py
@@ -1,6 +1,6 @@
 #para hackerram
-def pageCount(numero, pagina):
+def pageCount(n_Total, pagina):
     from_front = pagina // 2
-    from_back = (numero // 2) - (pagina // 2)
+    from_back = (n_Total // 2) - (pagina // 2)
     return min(from_front, from_back)
 
